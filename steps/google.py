c8d77aa236352699907d9def2b9b485427673f6f
eduardadalmas <eduardadalmas@ienh.com.br>
2023-09-18 21:46:19 -0300

    config browser

---
+++ steps/google.py
@@ -0,0 +1,20 @@
+from behave import *
+
+@given(u'que acesso a página do Google')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que acesso a página do Google')
+
+
+@given(u'que eu preencho a barra de pesquisar com Grêmio')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que eu preencho a barra de pesquisar com Grêmio')
+
+
+@when(u'eu clicar no botão de pesquisar')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When eu clicar no botão de pesquisar')
+
+
+@then(u'devo visualizar os resultados da pesquisa')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then devo visualizar os resultados da pesquisa')
